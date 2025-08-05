54be2dadc856766c01eba18773c206939b078f22
VishakEaswar <vishakeaswer@gmail.com>
2025-08-05 14:45:04 +0530

    4th update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { NavBar } from './components/NavBar';
 import HomePage from './pages/HomePage';
 // import LoginPage from './pages/LoginPage';
 // import SignInPage from './pages/SignInPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ContentPage from './pages/ContentPage';
 import DashboardPage from './pages/DashboardPage';
 import ProductLeads from './pages/ProductLeads';
 import AboutPage from './pages/AboutPage';
+
 const App = () => {
   return (
-    <Router>
+    <>
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -18,11 +19,41 @@ const App = () => {
         {/* <Route path="/signin" element={<SignInPage />} /> */}
         <Route path="/products" element={<ContentPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path='/productlead' element={<ProductLeads/>}/>
-        <Route path='/aboutPage' element={<AboutPage/>}/>
+        <Route path="/productlead" element={<ProductLeads />} />
+        <Route path="/aboutPage" element={<AboutPage />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
 export default App;
+
+
+// import React from 'react';
+// import { NavBar } from './components/NavBar';
+// import HomePage from './pages/HomePage';
+// // import LoginPage from './pages/LoginPage';
+// // import SignInPage from './pages/SignInPage';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import ContentPage from './pages/ContentPage';
+// import DashboardPage from './pages/DashboardPage';
+// import ProductLeads from './pages/ProductLeads';
+// import AboutPage from './pages/AboutPage';
+// const App = () => {
+//   return (
+//     <Router>
+//       <NavBar />
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/login" element={<LoginPage />} />z
+//         {/* <Route path="/signin" element={<SignInPage />} /> */}
+//         <Route path="/products" element={<ContentPage />} />
+//         <Route path="/dashboard" element={<DashboardPage />} />
+//         <Route path='/productlead' element={<ProductLeads/>}/>
+//         <Route path='/aboutPage' element={<AboutPage/>}/>
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
